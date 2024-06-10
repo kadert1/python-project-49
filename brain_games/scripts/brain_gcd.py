38596981b797e82dbbf723e325f4ad5bf5ce2cee
kaderti <vol4ckov.vladik@yandex.ru>
2024-06-10 21:02:46 +0300

    add brain_gcd.py

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games.logic_gcd import brain_gcd
+
+
+def main():
+    """the smallest common divisor."""
+    brain_gcd()
+
+
+if __name__ == '__main__':
+    main()
