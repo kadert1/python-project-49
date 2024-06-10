2a5c6168551507d2be519cba6cf327ac43453d9b
kaderti <vol4ckov.vladik@yandex.ru>
2024-06-10 21:28:54 +0300

    add brain_progression.py

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from brain_games.games.logic_progression import brain_progression
+
+
+def main():
+    """calculate the progression number"""
+    brain_progression()
+
+
+if __name__ == '__main__':
+    main()
